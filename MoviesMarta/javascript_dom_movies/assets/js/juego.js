2a9043069c000da4e30f25e861eaf59d3effa473
martafronton <martafronton@gmail.com>
2025-09-24 10:25:00 +0200

    Mostrar los recursos

--- MoviesMarta/javascript_dom_movies/assets/js/juego.js
+++ MoviesMarta/javascript_dom_movies/assets/js/juego.js
@@ -18,7 +18,7 @@ const getMoviesDeck = () => {
 const getElementsDeck = () => {
     let elementDeck = []
     for(let i = 1; i <= NMOVIES; i++) {
-        for(let j = 1; j <= NELEMENTSPMOVIE; j++) {
+        for(let j = 0; j < NELEMENTSPMOVIE; j++) {
             elementDeck.push("0"+i+"C"+j)
         } 
     }
@@ -41,13 +41,32 @@ const getRecurso = function(){
 
 
 let btNuevoJuego = document.getElementById('btNuevoJuego');
+let btAdivina = document.getElementById('btAdivina');
 let contenedorImagen = document.getElementById('pelicula-caratula');
+let contenedorRecursos = document.getElementById('elementos-pelicula');
+let contadorRecursos = 0;
 
 btNuevoJuego.addEventListener('click', function(event) {
     let movie = getMovie()
-    contenedorImagen.innerHTML = ` <img class="elemento" src="assets/movies/${movie}.jpg" alt="Carátula">    `
+    contenedorImagen.innerHTML = ` <img class="elemento" src="assets/movies/${movie}.jpg" alt="Carátula">`
+    contenedorRecursos.innerHTML =  
     event.stopPropagation();
 });
+
+btAdivina.addEventListener('click', function(event) {
+    let recurso = getRecurso()
+    contadorRecursos++
+    if((NELEMENTSPMOVIE)*(NMOVIES) > contadorRecursos){
+        contenedorRecursos.innerHTML += 
+        `<img class="elemento" src="assets/characters/${recurso}.jpg" alt="Recurso">`
+         event.stopPropagation();
+    }else{
+        alert("No hay más opciones")
+    }
+   
+});
+
+
 
  
 
